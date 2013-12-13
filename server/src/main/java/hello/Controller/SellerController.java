30afb58ff9e24efd4f4341f1034a65bb92973bc0
root <root@jokix-laptop.(none)>
2013-12-13 10:52:23 +0600

    Manas project commit

--- server/src/main/java/hello/Controller/SellerController.java
+++ server/src/main/java/hello/Controller/SellerController.java
@@ -47,6 +47,14 @@ public class SellerController {
         getRepository().delete(seller);
          return true;
     }
+    @RequestMapping(value = "/change/{name}", method = RequestMethod.PUT)
+    public @ResponseBody
+    boolean  changeUserPassword(@PathVariable("name") String name){
+        Iterable<Seller> seller = getRepository().findByFullName(name);
+        getRepository().save(seller);
+        return true;
+    }
+
 
     public SellerRepository getRepository() {
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
