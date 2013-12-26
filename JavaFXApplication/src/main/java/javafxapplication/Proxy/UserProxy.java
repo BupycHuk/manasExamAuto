60595d02a267b418ba76ad0fd5cbaa911b6615d7
askarbekov <askarbekov_aa@hotmail.com>
2013-12-26 18:01:38 +0600

    17

--- JavaFXApplication/src/main/java/javafxapplication/Proxy/UserProxy.java
+++ JavaFXApplication/src/main/java/javafxapplication/Proxy/UserProxy.java
@@ -9,6 +9,22 @@ import javafxapplication.Model.User;
  * Time: 2:03
  * To change this template use File | Settings | File Templates.
  */
-public class UserProxy extends Proxy {
+public class CarProxy extends Proxy {
 
+    public boolean addSpare(AddSpareRequest spareAddRequest)
+    {
+        try {
+            restTemplate.postForObject(urlService.GetServerUrl("/addSpare/"), spareAddRequest, SpareofCar.class);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+        return true;
+
+    }
+
+    public Cars[] getCars() {
+        return restTemplate.getForObject(urlService.GetServerUrl("/cars/"), Cars[].class);
+    }
 }
--- server/src/main/java/hello/Model/User.java
+++ server/src/main/java/hello/Model/User.java
@@ -17,4 +17,31 @@ public class User {
         this.id = Id;
     }
 }
+public class Cars
+{
+    int ID;
+    String carName;
+    public Cars (String cname)
+    {
+        name=cname;
+    }
+    public static void main(String[] args)
+    {
+        Cars myCars=new Cars ("Volvo")
 
+    }
+}
+public class SpareOfCar
+{
+}
+public class HelloClient {
+    public static void main(String[] args) {
+        try {
+            Hello proxy = (Hello)(new HelloFriend().getHelloIF());
+            proxy._setTargetEndpoint(args[0]);
+            System.out.println(proxy.sayHello("Hello"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
